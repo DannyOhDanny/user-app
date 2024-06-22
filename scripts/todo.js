f6b3053529b50391f87a1c1ebf713de9e03f00f5
Daria Matveeva <119957276+DannyOhDanny@users.noreply.github.com>
2024-06-22 21:11:29 +0300

    Feat: styles update, todo script update

--- scripts/todo.js
+++ scripts/todo.js
@@ -77,3 +77,12 @@ document.addEventListener('mouseout', function (event) {
     hideTooltip();
   }
 });
+
+const currentDate = new Date();
+const day = String(currentDate.getDate()).padStart(2, '0');
+const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+const year = String(currentDate.getFullYear()).slice(-2);
+const formattedDate = `${day}.${month}.${year}`;
+
+const header = document.querySelector('.todo-container__header');
+header.textContent += ` на ${formattedDate}`;
